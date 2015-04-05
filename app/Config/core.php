2c912d8fa7f9ded37a6d834edfcbb8405a0442c1
clark <huajie.chen@utoronto.ca>
2015-04-05 01:00:57 -0400

    change security salt

--- app/Config/core.php
+++ app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ifjis39481id93j9d)(@37sk@91k(23j%192');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '3485737587847584790293497548976338457');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
