b063858f6d4738add84cf2a4a75a6f0be87c098e
clark <huajie.chen@utoronto.ca>
2015-04-05 02:35:49 -0400

    adding pages

--- app/Controller/PostsController.php
+++ app/Controller/PostsController.php
@@ -5,4 +5,15 @@ class PostsController extends AppController {
 	public function index(){
 		$this->set('posts', $this->Post->find('all'));
 	}
+
+	public function view($id = null){
+		if(!$id){
+			throw new NotFoundException(__('Invalid post'));
+		}
+		$post = $this->Post->findById($id);
+		if(!$post){
+			$this->set('post',$post);
+		}
+		$this->set('post',$post);
+	}
 }
